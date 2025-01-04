289a7e0ebcae87e1d1a2e793ea5e2a184a867040
Dev Gayashan <tharuka.gayashan.5@gmail.com>
2025-01-04 20:14:12 +0530

    multi module setup

---
+++ common-dto/src/test/java/com/projects/commondto/CommonDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.commondto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
